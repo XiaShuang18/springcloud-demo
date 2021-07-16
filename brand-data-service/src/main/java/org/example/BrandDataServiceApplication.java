23de44aff20fa32ee26a1cd5438d06c3472c4b71
redmibook15 <zachary_xia@163.com>
2021-07-16 22:49:42 +0800

    第六次提交 自己写了一个品牌微服务，在这个搭好的架子上，自己做一些简单的实例。添加上yml配置文件 下一步：开始学习gulimall商城项目的 初级部分了，

--- brand-data-service/src/main/java/org/example/BrandDataServiceApplication.java
+++ brand-data-service/src/main/java/org/example/BrandDataServiceApplication.java
@@ -2,6 +2,7 @@ package org.example;
 
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -13,6 +14,11 @@ public class BrandDataServiceApplication {
 
     public static void main(String[] args) {
 //启动这个微服务
+        int port = 8090;
+        System.out.println("品牌默认是8090端口");
+//        下一步考虑使用异步任务的方式 改进这个启动的方式
 
+        new SpringApplicationBuilder(BrandDataServiceApplication.class).properties("server.port=" + port).run(args);
+//        new SpringApplicationBuilder(BrandDataServiceApplication.class).properties("server.port=" + port).run(args);
     }
 }
