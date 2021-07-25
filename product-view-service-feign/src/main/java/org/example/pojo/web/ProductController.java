8a6e5f9a96fc32bf8e4458e764d50436284c9548
redmibook15 <zachary_xia@163.com>
2021-07-25 08:21:34 +0800

    增加配置中心和配置客户端，在视图微服务中获取版本信息，成功。

--- product-view-service-feign/src/main/java/org/example/pojo/web/ProductController.java
+++ product-view-service-feign/src/main/java/org/example/pojo/web/ProductController.java
@@ -1,9 +1,11 @@
 package org.example.pojo.web;
+
 import java.util.List;
 
 import org.example.pojo.pojo.Product;
 import org.example.pojo.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,14 +13,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
- 
-	@Autowired
+
+    @Autowired
     ProductService productService;
-	
+    @Value("${version}")
+    String version;
+
     @RequestMapping("/products")
     public Object products(Model m) {
-    	List<Product> ps = productService.listProducts();
-    	m.addAttribute("my_products", ps);
+        List<Product> ps = productService.listProducts();
+        m.addAttribute("my_products", ps);
+        m.addAttribute("version", version);
         return "products";
     }
 }
